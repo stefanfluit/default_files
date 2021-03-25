1150a161adfd564eb660ec053906637cfc7f71de
Stefan Fluit <stefan@fluit-online.nl>
2021-03-25 14:35:41 +0100

    Add proper dir

--- terminal_files_fluit/auto_backup/backup_files.sh
+++ terminal_files_fluit/auto_backup/backup_files.sh
@@ -3,15 +3,16 @@
 set -o errexit      #Exit on uncaught errors
 
 declare USER_="fluit"
-declare BACKUP_DIR="/home/fluit/Documents/scripts/default_files/terminal_files_fluit/backup_${DATE_STAMP}"
-
-mkdir "${BACKUP_DIR}"
 
 # Define how many daily backups you want to keep. 
 declare retention_days="7"
 
 declare DATE_STAMP
 DATE_STAMP="$(date +%m-%d-%Y)"
+
+declare BACKUP_DIR="/home/fluit/Documents/scripts/default_files/terminal_files_fluit/backup_${DATE_STAMP}"
+
+mkdir "${BACKUP_DIR}"
 
 declare -a base_files=(
     "/home/${USER_}/.zshrc"
