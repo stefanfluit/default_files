2df93d491f36b99d1a7530b765b58d0c67e0811b
Stefan Fluit <stefan@fluit-online.nl>
2021-03-25 14:30:55 +0100

    Removed dir

--- terminal_files_fluit/auto_backup/backup_files.sh
+++ terminal_files_fluit/auto_backup/backup_files.sh
@@ -11,30 +11,23 @@ declare retention_days="7"
 declare DATE_STAMP
 DATE_STAMP="$(date +%m-%d-%Y)"
 
-mkdir "${BACKUP_DIR}/${DATE_STAMP}_backup"
-
 declare -a base_files=(
-    "/home/${USER_}/.zshrc"
+    "/home/${USER_}/.zshrc.${DATE_STAMP}"
     "/home/${USER_}/.p10k.zsh"
 )
 
-dconf dump /com/gexperts/Tilix/ > "${BACKUP_DIR}/${DATE_STAMP}_backup/tilix.dconf"
+dconf dump /com/gexperts/Tilix/ > "${BACKUP_DIR}/_backup/tilix.dconf"
 
 for file in "${base_files[@]}"; do
-    cp "${file}" "${BACKUP_DIR}/${DATE_STAMP}_backup"
+    cp "${file}" "${BACKUP_DIR}"
 done
 
 cd "${BACKUP_DIR}"
 
-git add "${BACKUP_DIR}/${DATE_STAMP}_backup" && git commit -m "Backup of ${DATE_STAMP}"
+git add "${BACKUP_DIR}/.zshrc" && git commit -m "Backup of ${DATE_STAMP} for .zshrc."
 
-git add "${BACKUP_DIR}/${DATE_STAMP}_backup/.zshrc" && git commit -m "Backup of ${DATE_STAMP} for .zshrc."
+git add "${BACKUP_DIR}/tilix.dconf" && git commit -m "Backup of ${DATE_STAMP} for tilix conf."
 
-git add "${BACKUP_DIR}/${DATE_STAMP}_backup/tilix.dconf" && git commit -m "Backup of ${DATE_STAMP} for tilix conf."
+git add "${BACKUP_DIR}/.p10k.zsh" && git commit -m "Backup of ${DATE_STAMP} for p10k config."
 
-git add "${BACKUP_DIR}/${DATE_STAMP}_backup/.p10k.zsh" && git commit -m "Backup of ${DATE_STAMP} for p10k config."
-
-cd "${BACKUP_DIR}/${DATE_STAMP}_backup" && GIT_SSH_COMMAND='ssh -i  /home/fluit/.ssh/id_ed25519_git.pub' git push
-
-# Removing the old files, except the value of retention_days. 
-#find "${BACKUP_DIR}" -type f -printf '%T@\t%p\n' | sort -t $'\t' -g | head -n -"${retention_days}" | cut -d $'\t' -f 2- | xargs rm -rf
+cd "${BACKUP_DIR}" && GIT_SSH_COMMAND='ssh -i  /home/fluit/.ssh/id_ed25519_git.pub' git push
