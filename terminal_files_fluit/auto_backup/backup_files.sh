424749c29ede3475ecb910cc12ce2306fb878a85
Stefan Fluit <stefan@fluit-online.nl>
2021-03-25 14:25:26 +0100

    Create directory for daily backups

--- terminal_files_fluit/auto_backup/backup_files.sh
+++ terminal_files_fluit/auto_backup/backup_files.sh
@@ -8,23 +8,27 @@ declare BACKUP_DIR="/home/fluit/Documents/scripts/default_files/terminal_files_f
 declare DATE_STAMP
 DATE_STAMP="$(date +%m-%d-%Y)"
 
+mkdir "${BACKUP_DIR}/${DATE_STAMP}_backup"
+
 declare -a base_files=(
     "/home/${USER_}/.zshrc"
     "/home/${USER_}/.p10k.zsh"
 )
 
-dconf dump /com/gexperts/Tilix/ > "${BACKUP_DIR}/tilix.dconf"
+dconf dump /com/gexperts/Tilix/ > "${BACKUP_DIR}/${DATE_STAMP}_backup/tilix.dconf"
 
 for file in "${base_files[@]}"; do
-    cp "${file}" "${BACKUP_DIR}"
+    cp "${file}" "${BACKUP_DIR}/${DATE_STAMP}_backup"
 done
 
 cd "${BACKUP_DIR}"
 
-git add "${BACKUP_DIR}/.zshrc" && git commit -m "Backup of ${DATE_STAMP} for .zshrc."
+git add "${BACKUP_DIR}/${DATE_STAMP}_backup" && git commit -m "Backup of ${DATE_STAMP}"
 
-git add "${BACKUP_DIR}/tilix.dconf" && git commit -m "Backup of ${DATE_STAMP} for tilix conf."
+git add "${BACKUP_DIR}/${DATE_STAMP}_backup/.zshrc" && git commit -m "Backup of ${DATE_STAMP} for .zshrc."
 
-git add "${BACKUP_DIR}/.p10k.zsh" && git commit -m "Backup of ${DATE_STAMP} for p10k config."
+git add "${BACKUP_DIR}/${DATE_STAMP}_backup/tilix.dconf" && git commit -m "Backup of ${DATE_STAMP} for tilix conf."
+
+git add "${BACKUP_DIR}/${DATE_STAMP}_backup/.p10k.zsh" && git commit -m "Backup of ${DATE_STAMP} for p10k config."
 
 cd "${BACKUP_DIR}" && GIT_SSH_COMMAND='ssh -i  /home/fluit/.ssh/id_ed25519_git.pub' git push
