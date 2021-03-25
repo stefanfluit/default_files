567b91bb0fe6b3703f5b23f4681ba6de03a8206c
Stefan Fluit <stefan@fluit-online.nl>
2021-03-25 13:58:24 +0100

    Add script for auto backup of my configs

---
+++ terminal_files_fluit/auto_backup/backup_files.sh
@@ -0,0 +1,33 @@
+#!/usr/bin/env bash
+
+set -o errexit      #Exit on uncaught errors
+
+declare USER_="fluit"
+declare BACKUP_DIR="/home/fluit/Documents/scripts/default_files/terminal_files_fluit"
+
+declare DATE_STAMP
+DATE_STAMP="date +%m-%d-%Y"
+
+declare -a base_files=(
+    "/home/${USER_}/.zshrc"
+    "/home/${USER_}/.p10k.zsh"
+)
+
+dconf dump /com/gexperts/Tilix/ > "${BACKUP_DIR}/tilix.dconf"
+
+for file in "${base_files[@]}"; do
+    cp "${file}" "${BACKUP_DIR}"
+done
+
+cd "${BACKUP_DIR}"
+
+git add "${BACKUP_DIR}/.zshrc" && \
+git commit -m "Backup of ${DATE_STAMP} for .zshrc."
+
+git add "${BACKUP_DIR}/.tilix.dconf" && \
+git commit -m "Backup of ${DATE_STAMP} for tilix conf."
+
+git add "${BACKUP_DIR}/.p10k.zsh" && \
+git commit -m "Backup of ${DATE_STAMP} for p10k config."
+
+GIT_SSH_COMMAND='ssh -i  ~/.ssh/id_ed25519_git.pub' git push
---
+++ terminal_files_fluit/auto_backup/install.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+
+set -o errexit      #Exit on uncaught errors
+
+# Finding the directory we're in
+declare DIR
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" &> /dev/null && pwd )"
+
+sudo cp "${DIR}/systemd/backup-files.service" "/etc/systemd/system/backup-files.service" && printf "Copied Sysd unit\n"
+sudo cp "${DIR}/systemd/backup-files.timer" "/etc/systemd/system/backup-files.timer" && printf "Copied Sysd unit\n"
+sudo systemctl daemon-reload
+sudo systemctl enable backup-files.timer --now
+
+mkdir /var/lib/scripts
+sudo cp "${DIR}/backup_files.sh" "/var/lib/scripts/backup_files.sh"
+sudo chmod +x "/var/lib/scripts/backup_files.sh"
+sudo chown -R ${USER_}:${USER_} "/var/lib/scripts"
